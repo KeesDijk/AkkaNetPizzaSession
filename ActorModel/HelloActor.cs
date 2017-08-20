8c748c6d793648859816e2367b661ffb5a82d6a5
Kees Dijk <kees.dijk@home.nl>
2017-08-20 16:18:36 +0200

    Initial untyped actor for HelloActor

---
+++ ActorModel/HelloActor.cs
@@ -0,0 +1,21 @@
+﻿using Akka.Actor;
+using System;
+
+namespace ActorModel
+{
+    public class HelloActor : UntypedActor
+    {
+        protected override void OnReceive(object message)
+        {
+            var msg = message as string;
+            if (msg != null)
+            {
+                Console.WriteLine("Hello: {0}", msg);
+            }
+            else
+            {
+                Console.WriteLine("Huh ?");
+            }
+        }
+    }
+}
---
+++ ChatServerHost/Program.cs
@@ -0,0 +1,23 @@
+﻿using ActorModel;
+using Akka.Actor;
+using System;
+
+namespace ChatServerHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ActorSystem system = ActorSystem.Create("MyChatServer");
+            IActorRef actorRef = system.ActorOf<HelloActor>();
+
+            actorRef.Tell("Kees");
+            actorRef.Tell(11);
+
+            Console.WriteLine("press any key");
+            Console.ReadLine();
+            system.Terminate();
+           
+        }
+    }
+}
