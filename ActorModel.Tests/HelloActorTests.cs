028814e88a4f3070306c81fc2953e4d32f2f4020
Kees Dijk <kees.dijk@home.nl>
2017-09-03 12:40:00 +0000

    Added the rest of the test project

---
+++ ActorModel.Tests/HelloActorTests.cs
@@ -0,0 +1,21 @@
+﻿using Akka.TestKit;
+using Akka.TestKit.Xunit2;
+using Xunit;
+
+namespace ActorModel.Tests
+{
+    public class HelloActorTests : TestKit
+    {
+        [Fact]
+        public void ShouldBeAbleToStartHelloActor()
+        {
+            //Arrange
+            TestActorRef<HelloActor> actor = ActorOfAsTestActorRef<HelloActor>();
+
+            //Act
+            actor.Tell("Hallo");
+
+            //Assert
+        }
+    }
+}
