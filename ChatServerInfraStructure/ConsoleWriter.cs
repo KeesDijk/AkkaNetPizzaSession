8ba7156826df20216f64f4404826fe5282ec272f
Kees Dijk <kees.dijk@home.nl>
2017-09-03 13:38:27 +0000

    Added rhe rest of ninject and unit test (getting used to gmaster)

---
+++ ChatServerInfraStructure/ConsoleWriter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChatServerInfraStructure
+{
+    public class ConsoleWriter : IWriter
+    {
+        public void WriteLine(string message, params object[] args)
+        {
+            Console.WriteLine(message, args);
+        }
+    }
+}
---
+++ ChatServerInfraStructure/IWriter.cs
@@ -0,0 +1,7 @@
+﻿namespace ChatServerInfraStructure
+{
+    public interface IWriter
+    {
+        void WriteLine(string message, params object[] args);
+    }
+}
